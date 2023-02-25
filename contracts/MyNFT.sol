5c69bdfcc4482a798b0f557e70c0e4f668e0c509
Pablo <pablo@hashingsystems.com>
2023-02-24 21:22:39 -0600

    totalSupply

--- contracts/MyNFT.sol
+++ contracts/MyNFT.sol
@@ -7,6 +7,7 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 contract MyNFT is ERC721 {
     address public otherContract;
     uint256 public requiredBalance;
+    uint256 public total;
 
     constructor(address _otherContract) ERC721("MyNFT", "NFT") {
         otherContract = _otherContract;
@@ -16,7 +17,6 @@ contract MyNFT is ERC721 {
     /**
      * @dev Modifier to check if the user has a valid balance in another contract.
      * @param account The user account to check the balance for.
-     * @return A boolean indicating if the user has a valid balance.
      */
     modifier onlyValid(address account) {
         require(checkBalance(account), "MyNFT: Caller does not have a valid balance");
@@ -29,7 +29,14 @@ contract MyNFT is ERC721 {
      * @return A boolean indicating if the user has the required balance.
      */
     function checkBalance(address account) public view returns (bool) {
-        (bool success, bytes memory data) = otherContract.call(abi.encodeWithSignature("balanceOf(address)", account));
+        (
+            bool success, 
+            bytes memory data
+        ) = 
+        otherContract.staticcall(
+            abi.encodeWithSignature("balanceOf(address)", account)
+        );
+
         require(success, "MyNFT: Failed to check balance");
         uint256 balance = abi.decode(data, (uint256));
         return balance >= requiredBalance;
@@ -40,8 +47,8 @@ contract MyNFT is ERC721 {
      * @return The ID of the newly minted NFT.
      */
     function mint() public onlyValid(msg.sender) returns (uint256) {
-        uint256 newItemId = totalSupply() + 1;
-        _mint(msg.sender, newItemId);
-        return newItemId;
+        total += 1;
+        _mint(msg.sender, total);
+        return total;
     }
 }
